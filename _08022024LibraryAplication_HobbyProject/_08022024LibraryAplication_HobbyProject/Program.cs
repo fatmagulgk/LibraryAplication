65ce8fe47290aa2cf8340f900c9637e66d7c2183
Fatma Gül Gök <148465338+fatmagulgk@users.noreply.github.com>
2024-02-08 14:32:06 +0300

    BaseOption

---
+++ _08022024LibraryAplication_HobbyProject/_08022024LibraryAplication_HobbyProject/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _08022024LibraryAplication_HobbyProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
